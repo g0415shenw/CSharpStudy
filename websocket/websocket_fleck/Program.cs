aea215189b81fdf10440ad6feb5927678cc45596
g0415shenw <411320557@qq.com>
2022-02-16 18:18:29 +0800

    增加了websocket的测试demo

---
+++ websocket/websocket_fleck/Program.cs
@@ -0,0 +1,31 @@
+﻿using Fleck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace websocket_fleck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var server = new WebSocketServer("ws://0.0.0.0:8181");
+            server.Start(socket =>
+            {
+                socket.OnOpen = () => Console.WriteLine("Open!");
+                socket.OnClose = () => Console.WriteLine("Close!");
+                socket.OnMessage = (message) =>
+                {
+                    Console.WriteLine(message);
+                    socket.Send(message);
+                };
+            });
+
+            Console.ReadKey();
+
+        }
+    }
+}
