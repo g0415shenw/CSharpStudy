724dfc08fdf35ff0cbee0d0d3c0d1ebedbfe57a6
g0415shenw <411320557@qq.com>
2022-02-17 18:25:01 +0800

    增加了http的post和get的demo

---
+++ GrammarStudy/GrammarStudy/HttpClientTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrammarStudy
+{
+    public class HttpClientTest
+    {
+        HttpClient client = new HttpClient();
+
+        public void TestGet()
+        {
+            //增加自定义head
+            client.DefaultRequestHeaders.Add("token", "12345");
+            var url = "http://127.0.0.1:8080/TestGet?count=99";
+
+            var rsp = client.GetAsync(url).Result;
+            LogHelp.logger.Info(rsp.StatusCode);
+            LogHelp.logger.Info(rsp.Content.ReadAsStringAsync().Result);
+        }
+
+        public void TestPost()
+        {
+            //增加自定义head
+            client.DefaultRequestHeaders.Add("token", "12345");
+            var url = "http://127.0.0.1:8080/TestPost";
+
+            HttpContent httpContent = new StringContent("8888888888888");
+            var rsp = client.PostAsync(url,httpContent).Result;
+            LogHelp.logger.Info(rsp.StatusCode);
+            LogHelp.logger.Info(rsp.Content.ReadAsStringAsync().Result);
+        }
+
+    }
+}
---
+++ GrammarStudy/GrammarStudyTests/HttpClientTestTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GrammarStudy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrammarStudy.Tests
+{
+    [TestClass()]
+    public class HttpClientTestTests
+    {
+        [TestMethod()]
+        public void TestGetTest()
+        {
+            HttpClientTest httpClientTest = new HttpClientTest();
+            httpClientTest.TestGet();
+        }
+
+        [TestMethod()]
+        public void TestPostTest()
+        {
+            HttpClientTest httpClientTest = new HttpClientTest();
+            httpClientTest.TestPost();
+        }
+    }
+}
