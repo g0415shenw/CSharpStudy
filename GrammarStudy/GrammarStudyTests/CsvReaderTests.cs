3bb7cc99c1dde65567e5ab4afc1796e8a3092707
g0415shenw <411320557@qq.com>
2022-02-18 14:27:27 +0800

    增加了读取csv的demo

---
+++ GrammarStudy/GrammarStudyTests/CsvReaderTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GrammarStudy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace GrammarStudy.Tests
+{
+    [TestClass()]
+    public class CsvReaderTests
+    {
+        [TestMethod()]
+        public void readCSVTest()
+        {
+
+            // 调用方法
+            string FilePath_3 = "zuobiao.CSV";  // csv文件地址
+            DataTable data = new DataTable();
+            CsvReader.readCSV(FilePath_3, out data); // 调用函数
+
+            // 遍历table 中的所有数据
+            for (int i = 0; i < data.Rows.Count; i++)
+            {
+                for (int j = 0; j < data.Columns.Count; j++)
+                {
+                    object obj = data.Rows[i][j]; //i行j列的值 
+                    Console.WriteLine("mulRea11= {0}", obj);
+                }
+
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -11,4 +11,5 @@ C#编程相关测试代码仓库，主要用来保存实验性的功能
 2022-02-14 增加了 C#json 和对象互相转换的 demo  
 2022-02-16 增加了 C#json 获取当个节点值的 demo  
 2022-02-16 增加了 C#的 websocket 的 demo 基于 fleck  
-2022-02-17 增加了 HttpClient 的 get 和 post 的 demo
+2022-02-17 增加了 HttpClient 的 get 和 post 的 demo  
+2022-02-18 增加了读取csv的demo  
